2085d0716b10ddcde74c8aff7e2bf5fb094861f7
Vasily <vautkin@teknik.io>
2018-12-26 19:04:17 +0100

    Update appveyor, .gitignore and master build script

--- build_windows.cmd
+++ build_windows.cmd
@@ -5,28 +5,28 @@ REM Used for echos.
 SET top_level_activity=MASTER
 
 REM If scripts are chain called the original dirs should not be overwritten.
-IF NOT DEFINED original_dir (
-    SET original_dir=%CD%
-)
-IF NOT DEFINED project_dir (
-    REM Arg 0 (called %0) is the full path to the file.
-    REM https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/call#batch-parameters
-    REM As per above link, %~dp0 expands the zeroth arg to a drive letter and path only.
-    SET folder_path=%~dp0
-    REM Project dir is up two dirs from the file.
-    SET project_dir=%folder_path%
-)
+
+SET original_dir=%CD%
+
+
+REM Arg 0 (called %0) is the full path to the file.
+REM https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/call#batch-parameters
+REM As per above link, %~dp0 expands the zeroth arg to a drive letter and path only.
+SET folder_path=%~dp0
+REM Project dir is up two dirs from the file.
+SET project_dir=%folder_path%
+
 
 ECHO %top_level_activity%: Calling build script.
-CALL %project_dir%\build_scripts\win\build_windows.cmd
+CALL %project_dir%\build_scripts\win\build.cmd
 IF ERRORLEVEL 1 EXIT /B 1
 
 ECHO %top_level_activity%: Calling deployment script.
-CALL %project_dir%\build_scripts\win\deploy_windows.cmd
+CALL %project_dir%\build_scripts\win\deploy.cmd
 IF ERRORLEVEL 1 EXIT /B 1
 
 ECHO %top_level_activity%: Calling packaging script.
-CALL %project_dir%\build_scripts\win\package_windows.cmd
+CALL %project_dir%\build_scripts\win\package.cmd
 IF ERRORLEVEL 1 EXIT /B 1
 
 CD %original_dir%
