a3a9473933a0a56aa1d5001668f5c54bbe1bf31e
Vasily <vautkin@teknik.io>
2019-03-17 16:18:06 +0100

    Replace dedicated functions with single binary function
    There are only 1 or 2 calls to specific directories in the codebase,
    hence this is unnecessary and just leads to bloat.

--- src/utils/paths.h
+++ src/utils/paths.h
@@ -9,15 +9,7 @@ using std::string;
 
 optional<string> binaryDirectory();
 optional<string> binaryDirectoryFindFile( string fileName );
-
-optional<string> qmlDirectory();
-optional<string> qmlDirectoryFindFile( string fileName );
-
-optional<string> imageDirectory();
-optional<string> imageDirectoryFindFile( string fileName );
-
-optional<string> soundDirectory();
-optional<string> soundDirectoryFindFile( string fileName );
+optional<string> binaryDirectoryFindDirectory( string directoryName );
 
 optional<string> settingsDirectory();
 optional<string> settingsDirectoryFindFile( string fileName );
