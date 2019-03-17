6447760f6f0920370643b7230fba68de870c8a38
Vasily <vautkin@teknik.io>
2019-03-17 19:45:37 +0100

    Update Linux build script

--- build_scripts/linux/build_linux.sh
+++ build_scripts/linux/build_linux.sh
@@ -24,3 +24,9 @@ qmake --version
 qmake -spec $QMAKE_SPEC ..
 $MAKE_COMMAND
 $CLANG_TIDY
+cp -r ../src/res ./bin/linux/AdvancedSettings/
+cp ../src/package_files/action_manifest.json ./bin/linux/AdvancedSettings/
+cp ../src/package_files/manifest.vrmanifest ./bin/linux/AdvancedSettings/
+cp -r ../src/package_files/default_action_manifests ./bin/linux/AdvancedSettings/
+cp ../third-party/openvr/lib/linux64/libopenvr_api.so ./bin/linux/AdvancedSettings/
+cp ../build_scripts/linux/run-with-library.sh ./bin/linux/AdvancedSettings/
