afc9d71f6b6948ea7b7c9442319410cad471903a
Vasily <vautkin@teknik.io>
2019-03-17 12:10:58 +0100

    Add prototype for binary directory functions

--- src/utils/paths.cpp
+++ src/utils/paths.cpp
@@ -1,19 +1,37 @@
 #include "paths.h"
 #include <QStandardPaths>
+#include <QString>
+#include <easylogging++.h>
+
 namespace paths
 {
 optional<string> binaryDirectory()
 {
-    const auto binaryDir
+    const auto path
         = QStandardPaths::writableLocation( QStandardPaths::AppDataLocation );
 
-    if ( binaryDir != "" )
+    if ( path == "" )
     {
-        return binaryDir.toStdString();
+        LOG( ERROR ) << "Could not find binary directory.";
+        return std::nullopt;
     }
-    else
-    {
-        return {};
-    }
+
+    return path.toStdString();
 }
+
+optional<string> binaryDirectoryFindFile( const string fileName )
+{
+    const auto path = QStandardPaths::locate(
+        QStandardPaths::AppDataLocation, QString::fromStdString( fileName ) );
+
+    if ( path == "" )
+    {
+        LOG( ERROR ) << "Could not find file '" << fileName
+                     << "' in binary directory.";
+        return std::nullopt;
+    }
+
+    return path.toStdString();
+}
+
 } // namespace paths
