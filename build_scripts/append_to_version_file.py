9303d93f2764b20902a998b47f444fc4ae0318eb
Vasily <vautkin@teknik.io>
2019-04-19 18:08:49 +0200

    Make appveyor able to inject git commit into version

---
+++ build_scripts/append_to_version_file.py
@@ -0,0 +1,13 @@
+from sys import argv
+from os import path
+
+def get_version_string():
+    dir_path = path.dirname(path.realpath(__file__))
+    with open(dir_path + "/compile_version_string.txt", "r") as file:
+        contents = file.readline()
+        return contents.strip('"')    
+    
+if __name__ == "__main__":
+    current_version = get_version_string()
+    new_version = current_version + "-" + argv[1]
+    print("\"" + new_version + "\"")
