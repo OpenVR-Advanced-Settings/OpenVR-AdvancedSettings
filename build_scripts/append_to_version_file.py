72a319bc1edb34226c706ad29f12aff9a92adc3f
Vasily <vautkin@teknik.io>
2019-04-19 18:33:29 +0200

    Make Python replace version file responsible for file writes
    While the initial idea of having cmd/PS/bash actually write the changes
    to file, the integration with the AppVeyor YAML just made it really
    frustrating.

--- build_scripts/append_to_version_file.py
+++ build_scripts/append_to_version_file.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 from sys import argv
-from os import path
+from os import path, remove
+
+current_path = path.dirname(path.realpath(__file__))
+version_file_path = path.join(current_path, "compile_version_string.txt")
 
 def get_version_string():
-    dir_path = path.dirname(path.realpath(__file__))
-    with open(path.join(dir_path, "compile_version_string.txt"), "r") as file:
+    with open(version_file_path, "r") as file:
         contents = file.readline()
         return contents.strip('"')    
-    
+
+def set_version_string(new_version_string):
+    remove(version_file_path)
+    with open(version_file_path, "w") as file:
+        file.write(new_version_string)
+
 if __name__ == "__main__":
     current_version = get_version_string()
     new_version = current_version + "-" + argv[1]
-    print("\"" + new_version + "\"")
+    set_version_string(new_version)
