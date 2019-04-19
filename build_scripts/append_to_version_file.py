5c4e8a34861d656c5118e6bfd1cfea0baffc1f20
Vasily <vautkin@teknik.io>
2019-04-19 19:05:02 +0200

    Make version append script respect release builds
    Users do not care about commit numbers and it only serves to confuse
    them. Release builds should not contain build metadata since there
    should only be one definitive version.

--- build_scripts/append_to_version_file.py
+++ build_scripts/append_to_version_file.py
@@ -8,14 +8,21 @@ version_file_path = path.join(current_path, "compile_version_string.txt")
 def get_version_string():
     with open(version_file_path, "r") as file:
         contents = file.readline()
-        return contents.strip('"')    
+        return contents.strip('"')
 
 def set_version_string(new_version_string):
     remove(version_file_path)
     with open(version_file_path, "w") as file:
         file.write("\"" + new_version_string + "\"")
 
-if __name__ == "__main__":
+def main():
     current_version = get_version_string()
-    new_version = current_version + "+" + argv[1]
+    if "-release" in current_version.lower():
+        current_version.replace("-release", "")
+    else:
+        new_version = current_version + "+" + argv[1]
+
     set_version_string(new_version)
+
+if __name__ == "__main__":
+    main()
