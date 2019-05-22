b3307919f545550b8af3d8cfd5e74c4ef9939a2f
Vasily <vautkin@teknik.io>
2019-05-22 22:03:45 +0200

    Add dummy media_keys file
    This allows Linux (and MacOS) to compile correctly again. The previous
    behavior was an intended middle step.

---
+++ src/media_keys/media_keys_dummy.cpp
@@ -0,0 +1,25 @@
+#include "media_keys.h"
+
+namespace keyboardinput
+{
+void sendMediaNextSong()
+{
+    // dummy
+}
+
+void sendMediaPreviousSong()
+{
+    // dummy
+}
+
+void sendMediaPausePlay()
+{
+    // dummy
+}
+
+void sendMediaStopSong()
+{
+    // dummy
+}
+
+} // namespace keyboardinput
