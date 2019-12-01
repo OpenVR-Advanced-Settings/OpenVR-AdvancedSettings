24e3081533b2520a4e4cdd4c266ae4f89aabc687
Vasily <vautkin@teknik.io>
2019-12-01 15:36:46 +0100

    Convert m_oldStyleMotion to new settings API

--- src/tabcontrollers/MoveCenterTabController.h
+++ src/tabcontrollers/MoveCenterTabController.h
@@ -170,7 +170,6 @@ private:
     bool m_dashWasOpenPreviousFrame = false;
     bool m_roomSetupModeDetected = false;
     bool m_seatedModeDetected = false;
-    bool m_oldStyleMotion = false;
     bool m_universeCenteredRotation = false;
     bool m_enableSeatedMotion = false;
     bool m_simpleRecenter = false;
